0655081be06f9ac8816290e3938893976b0d2cd4
gonzjv <gonzjv@ya.ru>
2024-10-28 14:12:53 +0200

    test: get Kth elem to last from LinkedList

--- src/test/java/com/trshkv/linked_list_utils/LinkedListUtilsTest.java
+++ src/test/java/com/trshkv/linked_list_utils/LinkedListUtilsTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.trshkv.linked_list_utils.LinkedListUtils.LinkedListNode;
+
 public class LinkedListUtilsTest {
     
     @Test
@@ -23,5 +25,17 @@ public class LinkedListUtilsTest {
         assertEquals(Integer.valueOf(1), list.get(0));
         assertEquals(Integer.valueOf(2), list.get(1));
         assertEquals(Integer.valueOf(3), list.get(2));
+    }
+
+    @Test
+    public void shouldReturnKthElemToLast() {
+        LinkedListUtils utils = new LinkedListUtils();
+
+        LinkedListNode<Integer> node4 = utils.new LinkedListNode<Integer>(4, null);
+        LinkedListNode<Integer> node3 = utils.new LinkedListNode<Integer>(3, node4);
+        LinkedListNode<Integer> node2 = utils.new LinkedListNode<Integer>(2, node3);
+        LinkedListNode<Integer> node1 = utils.new LinkedListNode<Integer>(1, node2);
+
+        assertEquals(Integer.valueOf(3), LinkedListUtils.getKthElementToLast(node1, 2).getData()); 
     }
 }
